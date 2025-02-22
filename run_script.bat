ac2d6e8c64c629643f406130cf604cbdc9d1c63c
ABHISHEK TEJA <96369890+Abhishekteja443@users.noreply.github.com>
2025-02-22 14:20:54 -0500

    Update run_script.bat

--- run_script.bat
+++ run_script.bat
@@ -1,8 +1,8 @@
 @echo off
-call C:\Users\gabhi\Projects\Auto_mail_sender\venv\Scripts\activate.bat
+call venv\Scripts\activate.bat
 
-python C:\Users\gabhi\Projects\Auto_mail_sender\resend.py >>C:\Users\gabhi\Projects\Auto_mail_sender\logfile.txt 2>&1
+python resend.py >> logfile.txt 2>&1
 
-call C:\Users\gabhi\Projects\Auto_mail_sender\venv\Scripts\deactivate.bat
+call venv\Scripts\deactivate.bat
 
 exit
